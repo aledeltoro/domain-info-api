ad48e6ecd8bbe258daefec81f398d7c3d5e7a70e
aledeltoro <alejandroencarnacion1@gmail.com>
2020-06-23 16:30:21 -0400

    feat: Add method to insert "Host" struct data to database

--- platform/hostinfo/host.go
+++ platform/hostinfo/host.go
@@ -3,6 +3,7 @@ package hostinfo
 import (
 	sslAPI "domain-info-api/platform/ssllabs"
 	scraping "domain-info-api/platform/webscraping"
+	"log"
 	"time"
 )
 
@@ -18,13 +19,13 @@ type Host struct {
 	CreatedAt      time.Time
 }
 
-var statusMessage = map[string]bool{
+var statusMessages = map[string]bool{
 	"ERROR": true,
 	"READY": false,
 }
 
-// CreateHost returns a new Host d based on the given url
-func CreateHost(domain string) *Host {
+// NewHost returns a new Host d based on the given url
+func NewHost(domain string) *Host {
 
 	var host Host
 
@@ -33,16 +34,52 @@ func CreateHost(domain string) *Host {
 	status := sslAPI.SslGet(domain).Status
 
 	host = Host{
-		Servers: servers,
+		Servers:        servers,
 		ServersChanged: false,
 		Grade:          GetLowestGrade(servers),
 		PreviousGrade:  "",
 		Logo:           siteInfo.Logo,
 		Title:          siteInfo.Title,
-		IsDown:         statusMessage[status],
+		IsDown:         statusMessages[status],
 		CreatedAt:      time.Now(),
 	}
 
 	return &host
 
 }
+
+// InsertHost inserts a record into the "host" database
+func (c *Connection) InsertHost(host *Host) {
+
+	insertHostStmt, err := c.DB.Prepare("INSERT INTO host (server_changed, ssl_grade, previous_ssl_grade, logo, title, is_down, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	record := insertHostStmt.QueryRow(host.ServersChanged, host.Grade, host.PreviousGrade, host.Logo, host.Title, host.IsDown, host.CreatedAt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lastInsertID int
+	record.Scan(&lastInsertID)
+
+	insertServerStmt, err := c.DB.Prepare("INSERT INTO server (address, ssl_grade, country, owner, host_id) VALUES ($1, $2, $3, $4, $5)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < len(host.Servers); i++ {
+
+		server := host.Servers[i]
+
+		_, err := insertServerStmt.Exec(server.Address, server.SslGrade, server.Country, server.Owner, lastInsertID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+}
+
+
