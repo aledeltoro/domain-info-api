540de726313e6d756b833690b2295cfd7a9840f9
Alejandro Encarnacion <alejandroencarnacion1@gmail.com>
2020-07-23 01:34:58 -0400

    Closes #10

---
+++ platform/hostinfo/server_test.go
@@ -0,0 +1,72 @@
+package hostinfo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetLowestGrade(t *testing.T) {
+
+	assertLowestGrade := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	var tests = []struct {
+		servers []Server
+		want    string
+	}{
+		{servers: []Server{
+			Server{
+				Address:  "server1",
+				SslGrade: "B",
+				Country:  "US",
+				Owner:    "Amazon.com, Inc.",
+			},
+			Server{
+				Address:  "server2",
+				SslGrade: "A+",
+				Country:  "US",
+				Owner:    "Amazon.com, Inc.",
+			},
+			Server{
+				Address:  "server3",
+				SslGrade: "A",
+				Country:  "US",
+				Owner:    "Amazon.com, Inc.",
+			},
+		}, want: "B"},
+		{servers: []Server{
+			Server{
+				Address:  "server1",
+				SslGrade: "A",
+				Country:  "US",
+				Owner:    "Cloudflare, Inc.",
+			},
+			Server{
+				Address:  "server2",
+				SslGrade: "A",
+				Country:  "US",
+				Owner:    "Cloudflare, Inc.",
+			},
+			Server{
+				Address:  "server3",
+				SslGrade: "A",
+				Country:  "US",
+				Owner:    "Cloudflare, Inc.",
+			},
+		}, want: "A"},
+	}
+
+	for _, test := range tests {
+		message := fmt.Sprintf("expected SSL grade: %s", test.want)
+		t.Run(message, func(t *testing.T) {
+			got := GetLowestGrade(test.servers)
+			assertLowestGrade(t, got, test.want)
+		})
+	}
+
+}
